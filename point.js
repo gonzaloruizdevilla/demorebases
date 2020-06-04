7ee220e13f4dbde275acbcbfdb5c74c030924971
Gonzalo Ruiz de Villa <gonzaloruizdevilla@gmail.com>
2020-06-04 13:37:06 +0200

    Polar and cartesian coordinates getters

--- point.js
+++ point.js
@@ -1,9 +1,27 @@
 export class Point {
+    #x
+    #y
     constructor(x, y) {
-      this.x = x;
-      this.y = y;
+      this.#x = x;
+      this.#y = y;
     }
-  
+
+    get r() {
+      return Math.hypot(this.#x, this.#y)
+    }
+
+    get theta() {
+      return this.#y ? Math.atan(this.#x /this.#y) : 0
+    }
+
+    get x() {
+      return this.#x;
+    }
+
+    get y() {
+      return this.y;
+    }
+
     static distance(a, b) {
       const dx = a.x - b.x;
       const dy = a.y - b.y;
